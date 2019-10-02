9c165141f19a5cbee8216ec95408195a02ad8960
ligorio93 <edwin@devf.mx>
2019-10-01 19:57:00 -0500

    adding upload logic

--- src/resolvers/PostResolvers/Mutation.js
+++ src/resolvers/PostResolvers/Mutation.js
@@ -1,18 +1,33 @@
 const { createPost, updatePost, deletePost } = require('../../services/PostService');
+const storage = require('../../utils/storage');
 //const { getOneAuthor } = require('../../services/AuthorService'); 
 
 
 const createNewPost =  async(_,{data},{user}) => {
     data.author = user._id;
-    const post = await createPost(data);
     // Temporal solution
     //const author = await getOneAuthor(params.data.author);
+    if(data.cover){
+        const { createReadStream }  = await data.cover;
+        const stream = createReadStream();
+        const image = await storage({ stream });
+        console.log(image);
+        data = {...data,cover:image.url};
+    }
+    const post = await createPost(data);
     user.posts.push(post._id);
     user.save();
     return post;
 };
 
 const updateOnePost =  async(_,{id,data},{user}) =>{
+    if(data.cover){
+        const { createReadStream }  = await data.cover;
+        const stream = createReadStream();
+        const image = await storage({ stream });
+        console.log(image);
+        data = {...data,cover:image.url};
+    }
     const post = await updatePost(id,data,user);
     if(!post) throw new Error('Post not exist');
     return post;
